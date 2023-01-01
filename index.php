1d208f0458be2b101d998b11bfd7044374ff4fe6
imad <imadidaliouali@gmail.com>
2023-01-01 18:53:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="public/css/style.css?v=<?php echo time() ?>">
+</head>
+
+<body>
+
+    <script src="public/js/script.js?v=<?php echo time() ?>"></script>
+</body>
+
+</html>
